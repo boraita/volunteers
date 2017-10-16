12524eabcf8fe0e701d391a16744a29a26276a08
rafael <rafael@exo.works>
2017-10-16 13:08:50 +0200

    Update configurations heroku

--- src/main/java/com/volunteers/VolunteersApplication.java
+++ src/main/java/com/volunteers/VolunteersApplication.java
@@ -1,4 +1,4 @@
-package com.volunteers.volunteers;
+package com.volunteers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/volunteers/config/ConnectionBd.java
+++ src/main/java/com/volunteers/config/ConnectionBd.java
@@ -1,4 +1,4 @@
-package com.colunteers.config;
+package com.volunteers.config;
 
 import java.net.URI;
 import java.net.URISyntaxException;
